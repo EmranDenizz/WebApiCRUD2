7573933f8649d0c472e3e585978daa4628d2e63b
EmranDenizz <emran1604@gmail.com>
2021-02-26 13:43:57 +0300

    Repository added.
    Added customer repository.

---
+++ WebApiCRUD.Business/Repository/CustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiCRUD.Business.Request;
+
+namespace WebApiCRUD.Business.Repository
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAll();
+    }
+}
---
+++ WebApiCRUD.Business/Request/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiCRUD.Business.Request
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Country { get; set; }
+    }
+}
